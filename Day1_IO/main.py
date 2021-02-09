aa5c384278d1c8502e33aed3be2b132dfec41fe6
Martins <martins@techspace.lv>
2021-02-09 21:03:21 +0200

    day 1 samples

---
+++ Day1_IO/main.py
@@ -0,0 +1,22 @@
+
+if __name__ == '__main__':
+    print("Kā Jūs sauc?")
+    firstName = input()
+    print('Sveiki, ' + firstName + '!')
+
+    # piemeri
+
+    # question1 = 'Kā Jus sauc?'
+    #
+    # print(question1)
+    # answer = input()
+    # print(answer)
+    #
+    # question2 = 'Kāds ir Jūsu vecums?\n'
+    # answer2 = input(question2)
+    #
+    # print("aa"+"bb")
+
+
+
+
